43e560b0a6cd622a5d86944f88689942d0358789
24torky <24torky@gmail.com>
2024-06-24 14:36:25 +0300

    -Adjust strategy to proper naming

--- dependency_strategy/transient_dependency_strategy.py
+++ dependency_strategy/transient_dependency_strategy.py
@@ -10,26 +10,11 @@ class TransientDependencyStrategy(IDependencyStrategyInterface):
     def _custom_post_build_strategy(self, interface: ABC,
                                     concrete_class: object,
                                     key_lookup: object) -> None:
-        """Scoped post build strategy"""
-
-        if (key_lookup not
-                in self._interface_lifetime_registery_lookup):
-            self._interface_lifetime_registery_lookup[key_lookup] = {
-                interface: concrete_class
-            }
-        elif (interface not
-              in self._interface_lifetime_registery_lookup[key_lookup]):
-            self._interface_lifetime_registery_lookup[key_lookup].update({
-                interface: concrete_class
-            })
+        """Transient post build strategy"""
+        return None
 
     def _custom_pre_build_strategy(self,
                                    interface: ABC,
                                    key_lookup: object) -> Optional[object]:
         """Transient pre build strategy"""
-        if (key_lookup in self._interface_lifetime_registery_lookup and
-            interface in self._interface_lifetime_registery_lookup
-                [key_lookup]):
-            return (self._interface_lifetime_registery_lookup[key_lookup]
-                    [interface])
         return None
